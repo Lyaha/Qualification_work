5b1f56cc546f917ce815e99a6722febfc2c41490
Lyaha <omgthisisus@gmail.com>
2025-04-26 18:33:35 +0300

    feat(backend): add command for table user

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from '../../entity/user.entity';
+import { User } from '../entity/user.entity';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +9,25 @@ export class UsersController {
   @Get()
   findAll(): Promise<User[]> {
     return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() data: Partial<User>): Promise<User> {
+    return this.usersService.create(data);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() data: Partial<User>): Promise<User> {
+    return this.usersService.update(id, data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.usersService.remove(id);
   }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from '../../entity/user.entity';
+import { User } from '../entity/user.entity';
 
 @Injectable()
 export class UsersService {
@@ -12,5 +12,29 @@ export class UsersService {
 
   findAll(): Promise<User[]> {
     return this.usersRepository.find();
+  }
+
+  async findOne(id: string): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
+  }
+
+  async create(data: Partial<User>): Promise<User> {
+    const newUser = this.usersRepository.create(data);
+    return this.usersRepository.save(newUser);
+  }
+
+  async update(id: string, data: Partial<User>): Promise<User> {
+    const user = await this.findOne(id);
+    const updatedUser = this.usersRepository.merge(user, data);
+    return this.usersRepository.save(updatedUser);
+  }
+
+  async remove(id: string): Promise<void> {
+    const user = await this.findOne(id);
+    await this.usersRepository.remove(user);
   }
 }
